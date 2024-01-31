2ad6195533849178752697f9976283ea97015e2d
udemike <lushgradeinvestment@gmail.com>
2024-01-31 14:07:49 +0100

    master

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 app = express()
 const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/starkweb")
+mongoose.connect(process.env.MONGODB_URI)
  .then(()=> console.log("Connected to MongoDb"))
  .catch((err)=> console.log(err))
 
